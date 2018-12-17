33200d8d778dd52f7b9fddc5cc85484836aab1fa
vsbRih0054 <Nio3d@seznam.cz>
2018-12-17 16:09:00 +0100

    commit

--- app/src/main/java/com/example/jana/arenalikegame/MenuActivity.java
+++ app/src/main/java/com/example/jana/arenalikegame/MenuActivity.java
@@ -1,7 +1,10 @@
 package com.example.jana.arenalikegame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Button newGameBtn = (Button) findViewById(R.id.newGame);
+        newGameBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(MenuActivity.this, SkillsActivity.class));
+                }
+            }
+        );
     }
+
 }
---
+++ app/src/main/java/com/example/jana/arenalikegame/SkillsActivity.java
@@ -0,0 +1,13 @@
+package com.example.jana.arenalikegame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SkillsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_skills);
+    }
+}
